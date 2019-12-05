bb5318b92ce2c00b42e12da006d8ae5e15d1b18b
chenjianzhao <j.623487211@163.com>
2019-12-05 14:03:12 +0800

    feat: 自定义注解实现多实现类 Bean 的选择性注入

--- annotation-autowire/src/main/java/com/example/annotationautowire/AnnotationAutowireApplication.java
+++ annotation-autowire/src/main/java/com/example/annotationautowire/AnnotationAutowireApplication.java
@@ -3,6 +3,9 @@ package com.example.annotationautowire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 自定义注解实现多实现类 Bean 的选择性注入
+ */
 @SpringBootApplication
 public class AnnotationAutowireApplication {
 
