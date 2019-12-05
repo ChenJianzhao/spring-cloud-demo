b0cd1a835be4ffccbdc211e05e11d5957ed71a89
chenjianzhao <j.623487211@163.com>
2019-12-05 15:05:45 +0800

    feat: 业务异常

--- common/src/main/java/com/example/common/CommonApplication.java
+++ common/src/main/java/com/example/common/CommonApplication.java
@@ -3,7 +3,12 @@ package com.example.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+/**
+ * 业务异常定义
+ * 全局处理业务异常
+ * 业务异常不触发 Hystrix 断路，包装为 HystrixBadRequestException，再从其中解析出业务异常
+ */
+@SpringBootApplication
 public class CommonApplication {
 
 	public static void main(String[] args) {
